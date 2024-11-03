1739f9e26a059c401497cc98705f1eea54e77fb9
arjavjain5203 <arjavjain.jain1942@gmail.com>
2024-11-03 18:29:24 +0530

    footer responsive

--- java.js
+++ java.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const scrollList = document.getElementById('scroll-list');
     const scrollLeftBtn = document.getElementById('scroll-left');
     const scrollRightBtn = document.getElementById('scroll-right');
-    const scrollAmount = 150; // Adjust scroll distance as needed
+    const scrollAmount = 1600; // Adjust scroll distance as needed
 
     // Scroll left
     scrollLeftBtn.addEventListener('click', () => {
